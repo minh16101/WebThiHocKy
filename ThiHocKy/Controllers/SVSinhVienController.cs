f264be919788d0c2bd053ab84547f91708259369
Minh Hoang Nhat <Admin@LAPTOP-F6J06I0S.(none)>
2022-02-20 20:10:29 +0700

    first commit

---
+++ ThiHocKy/Controllers/SVSinhVienController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ThiHocKy.Models;
+
+namespace ThiHocKy.Controllers
+{
+    public class SVSinhVienController : ApiController
+    {
+        // GET: api/SVSinhVien
+        MongoDBConnect connect = new MongoDBConnect("ThiHocKy");
+        public void Get()
+        {
+            
+        }
+
+        // GET: api/SVSinhVien/5
+        [Route("api/SVSinhVien/{name}/")]
+        public int Get(string name)
+        {
+            int start = name.IndexOfAny(new[]{'@'});
+            int last = name.Length;
+            string new_name = name.Remove(start, last-start);
+            int count = 0;
+            foreach (char i in new_name)
+            {
+                if ('0' < i && i < '9')
+                {
+                    new_name = new_name.Remove(0, count+2);
+                    break;
+                }
+                count++;
+            }
+            int value = int.Parse(new_name);
+            return value;
+        }
+
+        // POST: api/SVSinhVien
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SVSinhVien/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SVSinhVien/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
